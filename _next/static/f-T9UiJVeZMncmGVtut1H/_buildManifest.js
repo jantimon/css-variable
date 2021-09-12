75f78b15da4fea06c66ca30f4d545c7bcf0143d6
jantimon <jantimon@users.noreply.github.com>
2021-09-12 18:51:26 +0000

    deploy: dea3aa525970f860ca3816cbcd5b4d255cb4b6e9

---
+++ _next/static/f-T9UiJVeZMncmGVtut1H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/662-65dcd6b82fff3af42b8f.js","static/css/10db91ca84ea2c4e00a8.css","static/chunks/pages/index-04405fa1f24130ff808a.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

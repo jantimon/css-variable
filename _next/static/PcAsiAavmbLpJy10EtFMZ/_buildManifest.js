2e756063dba1f2832a9484d3ec0da0092df5b32c
jantimon <jantimon@users.noreply.github.com>
2021-09-13 10:38:33 +0000

    deploy: 0e3276985c67ed20c738ffe3901c0f97662745e8

--- _next/static/PcAsiAavmbLpJy10EtFMZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/120-f2d9cb0eba74d78b56e9.js","static/css/393c4d407b25fadc885d.css","static/chunks/pages/index-85a1e744f973a0024eb2.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/szHjGvVytzSeioQNAkHhI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/120-f2d9cb0eba74d78b56e9.js","static/css/1dbcfef937771ac4a471.css","static/chunks/pages/index-e9062866d86141a8d298.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

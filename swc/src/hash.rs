c59183984dbccaae40c63671bceb92e223d7a09b
Martin Disch <martindisch@gmail.com>
2022-02-27 10:57:25 +0100

    Clippy lint: replace nth(0) with next()
    Since we're operating on an iterator this is equivalent.

--- swc/src/hash.rs
+++ swc/src/hash.rs
@@ -13,7 +13,7 @@ pub fn hash(data: impl AsRef<[u8]>, length: usize) -> String {
     hasher.update(data);
     let hash_base64 = encode_config(hasher.finalize(), base64::URL_SAFE_NO_PAD);
 
-    let first_char = hash_base64.chars().nth(0).unwrap();
+    let first_char = hash_base64.chars().next().unwrap();
     // Ensure that the identifier starts with [_a-zA-Z]
     let first_char = if first_char.is_digit(10) || first_char == '-' {
         '_'

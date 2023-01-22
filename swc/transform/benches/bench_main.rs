b06688a50e236f257838aeaa69494b83161a9eba
Martin Disch <martindisch@gmail.com>
2023-01-22 21:31:01 +0100

    Add benchmark with long variable name

--- swc/transform/benches/bench_main.rs
+++ swc/transform/benches/bench_main.rs
@@ -55,6 +55,55 @@ pub fn styled_page(c: &mut Criterion) {
     });
 }
 
+pub fn nested(c: &mut Criterion) {
+    let source_map: Lrc<SourceMap> = Default::default();
+    let source_file = source_map.new_source_file(
+        FileName::Custom("nested.js".into()),
+        include_str!("nested.js").into(),
+    );
+
+    let program = parser::parse_file_as_program(
+        &source_file,
+        Default::default(),
+        Default::default(),
+        None,
+        &mut vec![],
+    )
+    .unwrap();
+
+    c.bench_function("nested default", |b| {
+        b.iter_batched(
+            || {
+                (
+                    program.clone(),
+                    TransformVisitor::new(Default::default(), String::from("hashed")),
+                )
+            },
+            |(mut program, mut visitor)| program.visit_mut_with(&mut visitor),
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("nested display_name", |b| {
+        b.iter_batched(
+            || {
+                (
+                    program.clone(),
+                    TransformVisitor::new(
+                        Config {
+                            display_name: true,
+                            ..Default::default()
+                        },
+                        String::from("hashed"),
+                    ),
+                )
+            },
+            |(mut program, mut visitor)| program.visit_mut_with(&mut visitor),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
 pub fn short_hash(c: &mut Criterion) {
     c.bench_function("short filename hash", |b| {
         b.iter(|| transform::hash(black_box("fileName.ts")))
@@ -71,5 +120,5 @@ pub fn longer_hash(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, styled_page, short_hash, longer_hash);
+criterion_group!(benches, styled_page, nested, short_hash, longer_hash);
 criterion_main!(benches);

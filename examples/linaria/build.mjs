55c7eccf032be6641bc8e3b69d3590787189557c
Jan Nicklas <j.nicklas@me.com>
2024-05-16 19:25:13 +0200

    retry build

--- examples/linaria/build.mjs
+++ examples/linaria/build.mjs
@@ -1,6 +1,9 @@
 // Linaria fails on the very first build 🤷‍♂️
 // Therefore this script does it twice on error
-
 import {$} from 'execa';
 
-$({stdout: ['pipe', 'inherit']})`npm run build:next`.catch(() => $({stdout: ['pipe', 'inherit']})`npm run build:next`);
+$({stdout: ['pipe', 'inherit']})
+    `npm run build:next`
+.catch(() => $({stdout: ['pipe', 'inherit']})
+    `npm run build:next`
+);

2e670cf16acca845f9db6f06c64298f3d2dd6bdd
Remo Vetere <remo.vetere@gmail.com>
2024-10-15 17:57:55 +0200

    Cleanup

--- examples/styled-components/babel.config.js
+++ examples/styled-components/babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["next/babel"],
     plugins: [
-      "@babel/plugin-transform-runtime",
       ["css-variable/babel", { async: false }],
       ["babel-plugin-styled-components", { ssr: true }],
     ],
--- examples/styled-components/next.config.js
+++ examples/styled-components/next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   pageExtensions: ["page.tsx"],
-  webpack: (config, { isServer }) => {
-    config.resolve.extensionAlias = {
-      ".js": [".js", ".mjs"],
-    };
-    return config;
-  },
-  transpilePackages: ["css-variable"],
 };
